e29a407f6a006c948cb0cb01f915dcd2b71b5428
TalentedDeveloper19980105 <montecristo19980105@gmail.com>
2023-08-06 21:11:31 -0700

    react native travel app

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/**
+ * Created by nghinv on Wed Jun 23 2021
+ * Copyright (c) 2021 nghinv@lumi.biz
+ */
+
+import AppTourProvider, { AppTour } from './AppTourProvider';
+import AppTourStep from './AppTourStep';
+import { useAppTour, useEvent } from './hook';
+
+export {
+  AppTourProvider,
+  AppTourStep,
+  useAppTour,
+  AppTour,
+  useEvent,
+};
